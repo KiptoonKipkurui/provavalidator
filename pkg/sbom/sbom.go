2479f8c20ae2765f5d5a0f75d14d6b257b4e7529
Daniel Kiptoon <kiptoonkipkurui@gmail.com>
2026-01-01 18:52:01 +0000

    added functionality to generate SBOM
    Signed-off-by: Daniel Kiptoon <kiptoonkipkurui@gmail.com>

--- pkg/sbom/sbom.go
+++ pkg/sbom/sbom.go
@@ -5,8 +5,16 @@ import (
 	"fmt"
 )
 
-func ExtractSBOM(ctx context.Context, image string) error {
-	// TODO: integrate Syft library
+func ExtractSBOM(ctx context.Context, image string) (*ResolvedSBOM, error) {
 	fmt.Println("[sbom] Extracting SBOM for:", image)
-	return nil
+
+	genSbom, err := generateSBOMForImage(ctx, image)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate SBOM: %w", err)
+	}
+
+	fmt.Printf("Generated SBOM for image %s: %+v\n", image, genSbom)
+
+	return genSbom, nil
 }
