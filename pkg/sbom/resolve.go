7a4b71a9852c651c258ee7a2e14eb55d4a1525cc
Daniel Kiptoon <kiptoonkipkurui@gmail.com>
2026-04-21 11:08:43 +0100

    refactor(sbom): route image SBOM resolution through unified extractor

--- pkg/sbom/resolve.go
+++ pkg/sbom/resolve.go
@@ -10,15 +10,5 @@ type ResolveOptions struct {
 }
 
 func ResolveForImage(ctx context.Context, imageRef string) (*ResolvedSBOM, error) {
-	// Fallsback: generate SBOM on demand
-	gen, err := withRuntimeEnvironment(ctx, func(runCtx context.Context, _ resolvedRuntimeConfig) (*ResolvedSBOM, error) {
-		return generateSBOMForImage(runCtx, imageRef)
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	// TODO: future expansion: extract from
-	return gen, nil
+	return ExtractSBOM(ctx, imageRef)
 }
